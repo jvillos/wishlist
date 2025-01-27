dd3b8a58eb15c1e66b1e4701c6e736f66e8893d8
jvillos <jvillosl14@gmail.com>
2025-01-27 12:29:22 +0100

    end colors

--- src/Wishlist/WishlistItem.jsx
+++ src/Wishlist/WishlistItem.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function WishlistItem({ wish, onCompletedChange }) {
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    console.log("Component created/executed: " + wish.text);
+    let ageInterval;
+    if (!wish.completed) {
+      //if the wish is not completed we start a timer that is adding 1 each second to the age
+      ageInterval = setInterval(() => {
+        setAge((currentAge) => currentAge + 1);
+      }, 1000);
+    } else {
+      setAge(0);
+      clearInterval(ageInterval);
+    }
+  }, [wish.completed]);
+
   return (
     <li
       className={`wish-list__item
                 ${wish.completed ? "wish-list__item--done" : ""}
+                ${age >= 5 && age < 10 ? "wish-list__item--warn" : ""}
+                ${age >= 10 ? "wish-list__item--danger" : ""}
                 `}
     >
       <input
